07d00b0fe3b9dfa75cff7c4351cd2eae22a7c737
João Paulo <lopes_jpaulo@hotmail.com>
2019-06-18 16:04:00 -0300

    Componente relógio

--- src/componentes/MultiElementos.jsx
+++ src/componentes/MultiElementos.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import PrimeiroComponente from './PrimeiroComponente'
 
 export default props => (
     <React.Fragment>
+        <PrimeiroComponente valor="Primeiro Componente" />
         <h1>Parte 1</h1>
         <h2>Parte 2</h2>
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 import PrimeiroComponente from './componentes/PrimeiroComponente'
 import { CompA, CompB as B } from './componentes/DoisComponentes'
 import MultiElementos from './componentes/MultiElementos'
+import Clock from './componentes/ClockComponente'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
+        <Clock />
         <MultiElementos></MultiElementos>
         <CompA valor="01"/>
         <B valor="02"/>
